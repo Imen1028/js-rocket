8934077247819f3859048040a551d1ad4f32ff52
Imen1028 <rehabimen88@gmail.com>
2022-01-21 16:15:18 +0800

    Added the JS function examples

---
+++ example-variables.js
@@ -0,0 +1,13 @@
+// Explanation
+
+var favColor = 'blue';
+var myFavColors = ['blue', 'green', 'black'];
+var num_favorite_number = 3;
+var Imen_has_a_girlfriend = true;
+var ImenObject = {
+    firstName : 'Imen',
+    LastName : 'Mo',
+    RelationshipStatus : 'Single',
+    IsMale : true,
+    Age : 21
+}; 
