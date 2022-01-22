e992fc7fe5e69ce4c6bbdd2ca519bcabfeaf9307
Imen1028 <rehabimen88@gmail.com>
2022-01-22 11:11:07 +0800

    js practice-1

---
+++ js/example-function.js
@@ -0,0 +1,22 @@
+//Explanation Function
+var fullname = function (firstname, secondname){
+    return firstname + ' ' + secondname;
+}
+
+var Myname = "Imen";
+
+var area = function (width, height) {
+    return width * height;
+}
+
+var WhatIsMyFavColor = function () {
+    return false
+
+    return true
+
+    return 'blue';
+}
+
+var doSomething = function () {
+    console.log("Do something!!!");
+}
---
+++ js/if-statement.js
@@ -0,0 +1,39 @@
+//Explanation
+
+// || is equal to or
+var big = "boat boat"
+var BBB = "bbb"
+
+if (big == "boat boat" || BBB == "bbb") {
+    alert ("I love Big Boat Boat!!!");
+}
+
+// && is equal to and
+var big = "boat boat"
+var BBB = "bbb"
+
+if (big == "boat boat" && BBB == "bbb") {
+    alert ("I love Big Boat Boat!!!");
+}
+
+// if (name)
+var name1 = "Imen"
+var name2 = "Levi"
+
+if (name1 == "Imen") {
+    alert ("Yeah!!!");
+    } else if (name2 == "Levi") {
+    alert ("Leviii");
+    } else {
+    alert ("false!!!");
+    }
+
+//if (number)
+var number1 = 100;
+var number2 = 1000;
+
+if (number1 > number2) {
+    alert ("true!!!");
+} else {
+    alert ("false!!!");
+}
