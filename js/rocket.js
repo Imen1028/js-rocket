d69e2bf64b692c76cfcb1c5af60add96bd9c8849
Imen1028 <rehabimen88@gmail.com>
2022-01-24 15:33:03 +0800

    Made this fucking rocket!!

---
+++ js/rocket.js
@@ -0,0 +1,41 @@
+var timer = null;
+var countDownNumber = 10;
+
+var changeState = function(state) {
+    document.body.className = 'body-state'+state;
+    //I don't understand the logic of these three elements...
+    clearInterval(timer);
+    countDownNumber = 10;
+    document.getElementById('countDown').innerHTML = countDownNumber;
+
+    if (state == 2) {
+        timer = setInterval(function (){
+            /*
+            Why if I put "countDownNumber = countDownNumber - 1;" before 
+            " document.getElementById('countDown').innerHTML = countDownNumber;",
+            can make the program run faster???
+            */
+
+            countDownNumber = countDownNumber - 1;
+            document.getElementById('countDown').innerHTML = countDownNumber;
+            if (countDownNumber <=0) {
+                changeState(3);
+            }; 
+        }, 300);
+    } else if (state == 3) {
+        var success = setTimeout(function ()
+            {
+                //this math formula looks so cool!! 
+                var randomNumber = Math.round(Math.random()*10);
+
+                console.log('Random number:', randomNumber);
+
+                if (randomNumber > 5) {
+                    changeState(4);
+                } else {
+                    changeState(5);
+                };
+        }, 2000)
+        //2000 等於 2000ms = 2s
+    }
+}
